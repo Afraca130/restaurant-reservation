6b26eb3427bd70e7bd477d2595531523138d6e91
afraca130 <zokatn@gmail.com>
2025-03-11 17:18:31 +0900

    feat: util 설정

--- src/util/typeorm.ts
+++ src/util/typeorm.ts
@@ -1,14 +1,26 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { Restaurant } from '../entity/restaurant.entity';
-import { Menu } from '../entity/menu.entity';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER || 'user',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'restaurant_db',
-  entities: [Restaurant, Menu],
-  synchronize: true,
-};
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    const mysqlOption: MysqlConnectionOptions = {
+      type: 'mysql',
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USER'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_NAME'),
+      entities: [__dirname + '/../**/*.entity.{ts,js}'],
+      synchronize: true,
+    };
+
+    return {
+      ...mysqlOption,
+    };
+  }
+}
